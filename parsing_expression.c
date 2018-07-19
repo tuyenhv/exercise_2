82a9e0518191b9a71865955df49d19c4b0dfbde5
Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>
2018-07-19 13:47:08 +0700

    parsing_expression.c: Fix formatting follow coding rules
    Signed-off-by: Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>

--- parsing_expression.c
+++ parsing_expression.c
@@ -4,76 +4,67 @@
 
 const char * expressionToParse = "3*(2.5+3.5)";
 
-char peek()
-{
-    return *expressionToParse;
+char peek(){
+  return *expressionToParse;
 }
 
-char get()
-{
-    return *expressionToParse++;
+char get(){
+  return *expressionToParse++;
 }
 
 float expression();
 
-float number()
-{
-    char str_value[50];
-    int i = 0;
-    bool decimal_found = false;
-    char c;
-    while ((peek() >= '0' && peek() <= '9') || peek() == '.')
-    {
-        str_value[i] = get();
-        i++;
-    }
-    return atof(str_value);
+float number(){
+  char str_value[50];
+  int i = 0;
+  bool decimal_found = false;
+  char c;
+  while ((peek() >= '0' && peek() <= '9') || peek() == '.'){
+    str_value[i] = get();
+    i++;
+  }
+  return atof(str_value);
 }
 
-float factor()
-{
-    if (peek() >= '0' && peek() <= '9')
-        return number();
-    else if (peek() == '(')
-    {
-        get(); // '('
-        float result = expression();
-        get(); // ')'
-        return result;
-    }
-    else if (peek() == '-')
-    {
-        get();
-        return -factor();
-    }
-    return 0; // error
-}
-
-float term()
-{
-    float result = factor();
-    while (peek() == '*' || peek() == '/')
-        if (get() == '*')
-            result *= factor();
-        else
-            result /= factor();
+float factor(){
+  if (peek() >= '0' && peek() <= '9')
+    return number();
+  else if (peek() == '('){
+    get(); // '('
+    float result = expression();
+    get(); // ')'
     return result;
+  }
+  else if (peek() == '-'){
+    get();
+    return -factor();
+  }
+  return 0; // error
 }
 
-float expression()
-{
-    float result = term();
-    while (peek() == '+' || peek() == '-')
-        if (get() == '+')
-            result += term();
-        else
-            result -= term();
-    return result;
+float term(){
+  float result = factor();
+  while (peek() == '*' || peek() == '/')
+    if (get() == '*')
+      result *= factor();
+    else
+      result /= factor();
+  return result;
+}
+
+float expression(){
+  float result = term();
+  while (peek() == '+' || peek() == '-')
+    if (get() == '+')
+      result += term();
+    else
+      result -= term();
+  return result;
 }
 
 int main(void)
 {
-    float result = expression();
-    printf("result: %f \n", result);
-    return 0;
+  float result = expression();
+  printf("result: %f \n", result);
+  return 0;
 }
